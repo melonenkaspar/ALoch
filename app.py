9a2143c049015e4be8430cf9f2d8a2a366f314b8
melonenkaspar <melonenkaspar@gmail.com>
2026-06-23 22:01:38 +0200

    Modify app to use environment variable for port

--- app.py
+++ app.py
@@ -334,4 +334,6 @@ def make_move(game_id):
     return jsonify({'ok': True})
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    import os
+    port = int(os.environ.get('PORT', 10000))
+    app.run(host='0.0.0.0', port=port)
